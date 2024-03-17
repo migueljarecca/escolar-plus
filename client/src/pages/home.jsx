28a20afd54d3fe3f57a4d32bc6ee57a77d50edb2
miguel <miguel.jarecca@gmail.com>
2024-03-17 15:09:10 -0500

    se trabaja en el html del home de la page

---
+++ client/src/pages/home.jsx
@@ -0,0 +1,22 @@
+
+export const home = () => {
+
+    return (
+        <header className="header">
+            <a href="#" className="logo">LOGO</a>
+
+            <nav>
+                <p>cole</p>
+            </nav>
+
+            <div className="nav-right">
+                <div className="search-box">
+                    <p>Icon</p>
+                    <input type="text" placeholder="Buscar"/>
+                </div>
+                <p>icon</p>
+                <p>icon</p>
+            </div>
+        </header>
+    )
+}
