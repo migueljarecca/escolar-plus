a604a8ce75646e91f39ad530ef34aab600eaca64
miguel <miguel.jarecca@gmail.com>
2024-12-13 18:59:24 -0500

    se da estilos al footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -15,43 +15,55 @@ export const Footer = () => {
 
     return(
         <footer className='footer'>
-            <div className="footer-contact">
-                <h6>Contactame</h6>
-                <p>Miguel jarecca ccallo | {new Date().getFullYear()}</p>
+            <div className="footer-content">
+                <div className="footer-contact">
+                    <h6>Contactame</h6>
 
-                <h5>Telefono</h5>
-                    <a 
-                        href="https://wa.me/51970449496" 
-                        target='_blank' 
-                        title='https://wa.me/51970449496'
-                    >
-                        +51 970449496
-                    </a>
+                    <h5>Telefono</h5>
+                        <a 
+                            href="https://wa.me/51970449496" 
+                            target='_blank' 
+                            title='https://wa.me/51970449496'
+                        >
+                            +51 970449496
+                        </a>
 
-                <h5>Correo</h5>
-                    <a href="mailto:miguel.jarecca@gmail.com">
-                        miguel.jarecca@gmail.com
-                    </a>
+                    <h5>Correo</h5>
+                        <a href="mailto:miguel.jarecca@gmail.com">
+                            miguel.jarecca@gmail.com
+                        </a>
+                </div>
+
+                <div className="footer-social-media">
+                    <a href="https://web.facebook.com/miguel.jareccaccallo.1/?locale=es_LA" 
+                    target="_blank">
+                    <FontAwesomeIcon icon={faFacebookF}/></a>  
+                    <a href="https://www.linkedin.com/in/migueljarecca"
+                    target="_blank">
+                    <FontAwesomeIcon icon={faLinkedinIn}/></a>  
+                    <a href="https://github.com/MiguelJarecca" 
+                    target="_blank">
+                    <FontAwesomeIcon icon={faGithub}/></a>  
+                </div>
+
+                <div className="footer-home">
+                    <button type="button" onClick={handleScrollToTop}>
+                        inicio
+                    </button>
+                </div>  
             </div>
 
-            <div className="footer-social-media">
-                <a href="https://web.facebook.com/miguel.jareccaccallo.1/?locale=es_LA" 
-                target="_blank">
-                <FontAwesomeIcon icon={faFacebookF}/></a>  
-                <a href="https://www.linkedin.com/in/migueljarecca"
-                target="_blank">
-                <FontAwesomeIcon icon={faLinkedinIn}/></a>  
-                <a href="https://github.com/MiguelJarecca" 
-                target="_blank">
-                <FontAwesomeIcon icon={faGithub}/></a>  
-            </div>
+            <span></span>
+            
+            <div className="footer-design">
+                <h3>Tienda de uniformes escolares {new Date().getFullYear()}</h3>
+                <span>|</span>
 
-            <div className="footer-home">
-                <button type="button" onClick={handleScrollToTop}>
-                    inicio
-                </button>
+                <a href="https://migueljarecca.github.io/portafolio-miguel/" 
+                    target="_blank">
+                     Sitio web creado por MIGUEL JARECCA
+                </a>  
             </div>
-
 
         </footer>
     )
