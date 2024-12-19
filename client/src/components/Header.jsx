35698dc9007b9d2b7bd0e736ea69b141e6835a8c
miguel <miguel.jarecca@gmail.com>
2024-12-19 11:18:30 -0500

    se realiza cambios en el header pero aun no queda solucionado

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -13,7 +13,9 @@ export const Header = () => {
 
         const { login, user } = useAuth();
         const { schools } = useSchool();
-        const [isSticky, setIsSticky] = useState(false);
+
+        const [hidden, setHidden] = useState(false); // Estado para controlar la visibilidad del banner
+        const [lastScrollPosition, setLastScrollPosition] = useState(0); // Estado para almacenar la última posición de scroll
 
         const [userState, setUserState] = useState(user?.userLogged || initialUserForm);
         const [isActiveDropdown, setIsActiveOrder] = useState(false);
@@ -26,96 +28,107 @@ export const Header = () => {
             setIsActiveOrder(!isActiveDropdown);
         }
 
-        const handleScroll = () => {
-          const shouldBeSticky = window.scrollY > 50;
-          setIsSticky(shouldBeSticky);
-        };
-      
+     
         useEffect(() => {
-          window.addEventListener('scroll', handleScroll);
+
+          const handleScroll = () => {
+            const currentScrollPosition = window.scrollY;
       
-          // Limpiar el evento cuando el componente se desmonte
-          return () => {
-            window.removeEventListener('scroll', handleScroll);
+            if (currentScrollPosition > lastScrollPosition && currentScrollPosition > 50) {
+              setHidden(true); // Ocultar banner
+            } else if (currentScrollPosition < lastScrollPosition && currentScrollPosition < 50) {
+              setHidden(false); // Mostrar banner
+            }
+      
+            setLastScrollPosition(currentScrollPosition); // Actualizar posición del scroll
           };
-        }, []);
+      
+          window.addEventListener("scroll", handleScroll);
+      
+          // Limpieza del evento al desmontar el componente
+          return () => window.removeEventListener("scroll", handleScroll);
+
+        }, [lastScrollPosition]); // Dependencia para que el efecto se ejecute al cambiar la posición del scroll
+      
+      
 
     return (
-        <>  
-            {login.isAuth
-            ? ''
-            :
-            <article className="header-article">
-              
-                <div className="header-article-content">
-                    <NavLink to={"/user/login"}>Inicia sesión</NavLink>
-                    <span>|</span>
-                    <NavLink to={"/user/register"}>Crea una cuenta</NavLink>
-                </div>
+        <section className='section-navigation-container'>
 
-                <h4 className='header-article-h4'>Dirección de la tienda</h4>
-            </article>
+            <div className="div-navigation-container">
             
-            }
-            
-    
-            <header className={`header ${isSticky ? 'sticky' : ''}`}>
-
-                <a href="/" className="logo">LOGO</a>
-
-                <div className={`dropdown-header ${isActiveDropdown ? 'active' : ''}`}>
-                    <div className="select" onClick={handlerColegios}>
-                        <span className="selected">Colegios</span>
-                        <div className={`caret ${isActiveDropdown ? 'active' : ''}`}></div>
+                {login.isAuth
+                ? ''
+                :
+                <article className={`top-navigation ${hidden ? "top-navigation--hidden" : "top-navigation--visible"}`}>
+                
+                    <div className="top-navigation-content">
+                        <NavLink to={"/user/login"}>Inicia sesión</NavLink>
+                        <span>|</span>
+                        <NavLink to={"/user/register"}>Crea una cuenta</NavLink>
                     </div>
-                    <ul className={`ul-menu ${isActiveDropdown ? 'active' : ''}`}>
-                        {schools.map((item) => (
-                            <li key={item.id}>
-                                <NavLink to={`/uniforms/${item.id}`} onClick={handlerColegios}>
-                                    {item.name}
-                                </NavLink>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
 
-                <div className="nav-right">
-                    <div className="search-box">
-                        <FontAwesomeIcon icon={faMagnifyingGlass}/>
-                        <input type="text" placeholder="Buscar"/>
+                    <h4 className='top-navigation-h4'>Dirección de la tienda</h4>
+                </article>
+                }
+
+                <header className="header">
+
+                    <a href="/" className="logo">LOGO</a>
+
+                    <div className={`dropdown-header ${isActiveDropdown ? 'active' : ''}`}>
+                        <div className="select" onClick={handlerColegios}>
+                            <span className="selected">Colegios</span>
+                            <div className={`caret ${isActiveDropdown ? 'active' : ''}`}></div>
+                        </div>
+                        <ul className={`ul-menu ${isActiveDropdown ? 'active' : ''}`}>
+                            {schools.map((item) => (
+                                <li key={item.id}>
+                                    <NavLink to={`/uniforms/${item.id}`} onClick={handlerColegios}>
+                                        {item.name}
+                                    </NavLink>
+                                </li>
+                            ))}
+                        </ul>
                     </div>
-                    <div className='icon-box'>
 
-                    {
-                        login.isAuth 
-                        ?
-                        <NavLink to={'/perfil'}>
-                            <FontAwesomeIcon icon={faUser}> </FontAwesomeIcon>
-                            <span>Hola! {userState.name}</span>
-                        </NavLink> 
-                        :
-                        <NavLink to={'/user/login'}>
-                            <FontAwesomeIcon icon={faUser}> </FontAwesomeIcon>
-                            <span>Cuenta</span>
-                        </NavLink>
-                    }   
+                    <div className="nav-right">
+                        <div className="search-box">
+                            <FontAwesomeIcon icon={faMagnifyingGlass}/>
+                            <input type="text" placeholder="Buscar"/>
+                        </div>
+                        <div className='icon-box'>
 
-                        <NavLink to={'/wishlist'}>
-                            <FontAwesomeIcon icon={faHeart}/>
-                            <span>Favoritos</span>
-                        </NavLink>
-                        
-                        <NavLink to={'/cart'}>
-                            <FontAwesomeIcon icon={faBagShopping}/>
-                            <span>Carrito</span>
-                        </NavLink>
-                        
+                        {
+                            login.isAuth 
+                            ?
+                            <NavLink to={'/perfil'}>
+                                <FontAwesomeIcon icon={faUser}> </FontAwesomeIcon>
+                                <span>Hola! {userState.name}</span>
+                            </NavLink> 
+                            :
+                            <NavLink to={'/user/login'}>
+                                <FontAwesomeIcon icon={faUser}> </FontAwesomeIcon>
+                                <span>Cuenta</span>
+                            </NavLink>
+                        }   
+
+                            <NavLink to={'/wishlist'}>
+                                <FontAwesomeIcon icon={faHeart}/>
+                                <span>Favoritos</span>
+                            </NavLink>
+                            
+                            <NavLink to={'/cart'}>
+                                <FontAwesomeIcon icon={faBagShopping}/>
+                                <span>Carrito</span>
+                            </NavLink>
+                            
+                        </div>
                     </div>
-                </div>
-            </header>
+                </header>
 
-            {/* Header placeholder para evitar el salto */}
-            {isSticky && <div className="header-placeholder"></div>}
-        </>
+            </div>
+
+        </section>
     )
 }
