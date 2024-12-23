234f349340c4aaeb51e6db283a4cbb598b3ce642
miguel <miguel.jarecca@gmail.com>
2024-12-23 16:20:53 -0500

    se mejoralos estilos de wishlist

--- client/src/pages/Wishlist.jsx
+++ client/src/pages/Wishlist.jsx
@@ -3,6 +3,8 @@ import { json, NavLink } from "react-router-dom";
 import { Header } from "../components/Header"
 import { useCart } from "../hooks/useCart";
 import { useWishlist } from "../hooks/useWishlist"
+import { Footer } from '../components/Footer';
+
 
 export const Wishlist = () => {
 
@@ -19,66 +21,73 @@ export const Wishlist = () => {
     return(
         <>
             <Header />
-            <h2 className="wishlist-title">Favoritos</h2>
 
-            <main className="wishlist-main">
+            <section className="section-wishlist-container">
 
-                { wishlist && wishlist.length > 0 ? 
+                <h2 className="wishlist-title">Favoritos</h2>
 
-                    wishlist.map(item => (
-                        <div key={item.id} className="wishlist-box">
-                            
-                            <div className="div-img">
-                            <img src={`data:${item.image.mime};base64,${item.image.content}`} alt={item.image.name} />
-                            
-                            </div>
-                    
-                            <div className="div-text">
-                                <h2>{item.product}</h2>
-                                <p>{item.gender}</p>
-                                <p>TALLA: {item.size}</p>
-                                <h3>S/. {item.price}</h3>
+                <main className="wishlist-main">
 
-                                <button
-                                    type="submit"
-                                    onClick={()=> handleRemoveToWishlist(item.id)}
-                                >
-                                    Eliminar
-                                </button>
+                    { wishlist && wishlist.length > 0 ? 
 
-                                <button
-                                    type="submit"
-                                    onClick={() => onClickAddCart(item)}
+                        wishlist.map(item => (
+                            <div key={item.id} className="wishlist-box">
+                                
+                                <div className="div-img">
+                                    <img src={`data:${item.image.mime};base64,${item.image.content}`} alt={item.image.name} />
+                                
+                                </div>
+                        
+                                <div className="div-text">
+                                    <h2>{item.product}</h2>
+                                    <p>{item.gender}</p>
+                                    <p>TALLA: {item.size}</p>
+                                    <h3>S/. {item.price}</h3>
+
+                                    <button
+                                        type="submit"
+                                        onClick={()=> handleRemoveToWishlist(item.id)}
                                     >
-                                    Agregar al carrito    
-                                </button>
+                                        Eliminar
+                                    </button>
+
+                                    <button
+                                        type="submit"
+                                        onClick={() => onClickAddCart(item)}
+                                        >
+                                        Agregar al carrito    
+                                    </button>
+
+                                </div>
 
                             </div>
+                        ))
+                    :  
+                    (
+                        <div>No hay productos en la lista de favoritos</div>
+                    ) 
+                    }
 
-                            {/* <div className="div-price">
-                                <h3>S/. {item.price}</h3>
-                            </div> */}
-                        </div>
-                    ))
-                :  
-                  (
-                    <div>No hay productos en la lista de favoritos</div>
-                  ) 
-                }
+                </main>
 
-            </main>
-
-           
-                <div className="wishlist-button">  
+                <div className="wishlist-div-a">
+                
                     {wishlist.length > 0
                     ? 
                         (     
-                        <NavLink to={`/uniforms/${wishlist[wishlist.length -1].school.id}` }>Continuar Comprando</NavLink>
+
+                                <NavLink to={`/uniforms/${wishlist[wishlist.length -1].school.id}` }>
+                                    Continuar Comprando
+                                </NavLink>
                         ) 
                     :
                         ''
                     }
-                </div> 
+                </div>
+
+            </section>
+
+            <Footer />
 
         </>
     )
