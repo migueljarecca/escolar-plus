23f4ed50469967bc48dc608a7a7ac7759dfcbb7c
miguel <miguel.jarecca@gmail.com>
2024-12-31 14:16:20 -0500

    se trabaja el los errores de usuario desde el front

--- client/src/components/UserForm.jsx
+++ client/src/components/UserForm.jsx
@@ -2,12 +2,20 @@ import { useEffect, useState } from "react"
 import { useUsers } from "../hooks/useUsers";
 import { useAuth } from "../hooks/useAuth";
 
+const initialFrontErrors = {
+    name: '',
+    lastname: '',
+    email: '',
+    password: ''
+}
+
 export const UserForm = ({ userSelect }) => {
 
     const { initialUserForm, handlerAddUser, handlerUpdateUser } = useUsers();
     const { handleLogin } = useAuth();
 
     const [userForm, setUserForm] = useState(initialUserForm);
+    const [frontErrors, setFrontErrors] = useState(initialFrontErrors);
 
     const {id, name, lastname, email, password} = userForm;
 
@@ -24,11 +32,53 @@ export const UserForm = ({ userSelect }) => {
         setUserForm({
             ...userForm,
             [name]: value, 
-        })
+        });
+
+        setFrontErrors({
+            ...initialFrontErrors,
+            [name]: value
+        });
     }
+
+    const validateForm = () => {
+        const errors = {};
+
+        if (!name.trim()) {
+            errors.name = "Por favor ingrese un nombre."
+        } else if (name.length < 3) {
+            errors.name = "El nombre debe tener almenos tres caracteres."
+        }
+
+        if (!lastname.trim()) {
+            errors.lastname = "Por favor ingrese un apellido."
+        } else if (name.length < 3) {
+            errors.lastname = "El apellido debe tener almenos tres caracteres."
+        }
+
+        if (!email.trim()) {
+            errors.email = "Por favor ingrese una correo electrónico.";
+        } else if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+            errors.email = "El correo electrónico no es válido.";
+        }
+
+        if (!password.trim()) {
+            errors.password = "Por favor ingrese una contraseña.";
+        } else if (password.length < 5) {
+            errors.password = "La contraseña debe tener al menos 6 caracteres.";
+        }
+        return errors;
+    };
 
     const onSubmitUserChange = async (event) => {
         event.preventDefault();
+
+        const errors = validateForm();
+
+        if (Object.keys(errors).length > 0) {
+            setFrontErrors(errors);
+            return;
+        }
+        
         if (id === '') {
             //Enviamos los datos del user al Hook useUsers
             await handlerAddUser(userForm);
@@ -58,8 +108,7 @@ export const UserForm = ({ userSelect }) => {
                     onChange={onInputUserChange}
                     value={name}
                 />
-                <label className="form-user-label-error">Por favor ingrese un nombre</label>
-
+                {frontErrors.name && <span>{frontErrors.name}</span>}
 
                 <label htmlFor="userlastname" className="form-user-label">Apellido</label>
                 <input 
@@ -70,7 +119,7 @@ export const UserForm = ({ userSelect }) => {
                     onChange={onInputUserChange}
                     value={lastname}
                 />
-                <label className="form-user-label-error">Por favor ingrese un nombre</label>
+                {frontErrors.lastname && <span>{frontErrors.lastname}</span>}
 
                 <label htmlFor="useremail" className="form-user-label">Correo electrónico</label>
                 <input 
@@ -81,7 +130,7 @@ export const UserForm = ({ userSelect }) => {
                     onChange={onInputUserChange}
                     value={email}
                 />
-                <label className="form-user-label-error">Por favor ingrese un nombre</label>
+                {frontErrors.email && <span>{frontErrors.email}</span>}
 
                 {id > 0 
                 ? '' 
@@ -96,7 +145,7 @@ export const UserForm = ({ userSelect }) => {
                         onChange={onInputUserChange}
                         value={password}
                     />
-                    <label className="form-user-label-error">Por favor ingrese un nombre</label>
+                {frontErrors.password && <span>{frontErrors.password}</span>}
                 </>
                 }
                 
